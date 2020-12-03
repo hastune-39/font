d29acebe5bf7620058d50fdff62bccaad36fb6d9
丁士玉 <18373622@buaa.edu.cn>
2020-12-03 17:44:53 +0800

    loading collections...

--- src/components/picturelist/HomePagelist.vue
+++ src/components/picturelist/HomePagelist.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!--    <el-button type="primary" icon="el-icon-edit" circle @click="changestore"></el-button>-->
-<!--    <el-button type="success" icon="el-icon-check" circle @click="mydebug"></el-button>-->
+    <!--    <el-button type="success" icon="el-icon-check" circle @click="mydebug"></el-button>-->
     <el-row :gutter="20" class="flex">
       <el-col :span="6">
         <div class="grid-content bg-purple">
@@ -48,23 +48,49 @@
             title: String,
           }
         ],
-        list1: {
-          totalHeight: Number,
-          pictureURL: [],
-        },
-        list2: {
-          totalHeight: Number,
-          pictureURL: [],
-        },
-        list3: {
-          totalHeight: Number,
-          pictureURL: [],
-        },
+
+        collectionItems: [
+          {
+            user_id: Number,
+            picture_id: Number,
+            collectTime: String,
+          }
+        ],
+
+        lists: [
+          {
+            totalHeight: Number,
+            picturesMessage: [
+              {
+                width: Number,
+                height: Number,
+                picture: {
+                  picture_id: Number,
+                  painter_id: Number,
+                  picture_address: String,
+                  uploadTime: String,
+                  title: String,
+                },
+                collection: {
+                  user_id: Number,
+                  picture_id: Number,
+                  CollectTime: String,
+                }
+              }
+            ]
+          }
+        ],
         pictureWidth: 500,
       }
     },
 
-    computed: {},
+    computed: {
+      userID() {//监听用户id,从而对收藏内容的显示进行变更
+        //id变更时,进行主页collection显示的更新
+        this.selectAllCollections();
+        return this.$store.state.user.userID;
+      }
+    },
 
     watch: {
       '$route': 'search',
@@ -72,20 +98,36 @@
 
     // var: timer=setInterval(this.makeSureLoad, 50),
     methods: {
-      //loading picture function
-      imgload(url, callback) {
-        var img = new Image();
-        img.src = url;
-        // if (img.complete) {
-        //   //图片是否存在缓存
-        //   callback(img);
-        //   return;
-        // }
-        img.onload = function () {
-          callback(img);
+
+      //开始时的初始化，设置总共有listNum列pagelist
+      startInit(listNum) {
+        let _this = this;
+        for (let i = 0; i < listNum; i++) {
+          _this.lists.push(
+            {
+              totalHeight: 0,
+              picturesMessage: {
+                width: Number,
+                height: Number,
+                picture: {
+                  picture_id: Number,
+                  painter_id: Number,
+                  picture_address: String,
+                  uploadTime: String,
+                  title: String,
+                },
+                collection: {
+                  user_id: Number,
+                  picture_id: Number,
+                  CollectTime: String,
+                }
+              }
+            }
+          );
         }
       },
 
+      //还没有重写
       initialsubList() {//性能问题?
         this.list1.totalHeight = 0;
         this.list2.totalHeight = 0;
@@ -94,46 +136,81 @@
         this.list2.pictureURL = [];
         this.list3.pictureURL = [];
         console.log("查看清空效果");
-        console.log("displayItem的图片个数为: "+ this.displayItems.length);
+        console.log("displayItem的图片个数为: " + this.displayItems.length);
         console.log("list1图片个数为：" + this.list1.pictureURL.length);
         console.log("list2图片个数为：" + this.list2.pictureURL.length);
         console.log("list3图片个数为：" + this.list3.pictureURL.length);
         console.log('finish initialize');
       },
 
-      calculateImageHeight(width, height){
-        console.log("宽度是: "+ width, "高度是: "+height);
-        console.log("相对高度是: "+ ((this.pictureWidth)*height/width));
-        return ((this.pictureWidth)*height/width);
-      },
-
-      allocPicture(img) {
-        console.log('图片信息为: src——' + img.src + ", height——" + img.height + ';');
-        console.log('待插入的列表的信息为:list1: ' + this.list1.totalHeight + ' list2: ' + this.list2.totalHeight + ' list3: ' + this.list3.totalHeight);
-        if (this.list1.totalHeight <= this.list2.totalHeight &&
-          this.list1.totalHeight <= this.list3.totalHeight) {
-          this.list1.totalHeight += this.calculateImageHeight(img.width, img.height);
-          this.list1.pictureURL.push(img.src);
-          console.log('put in list1');
-        } else if (this.list2.totalHeight <= this.list3.totalHeight) {
-          this.list2.totalHeight += this.calculateImageHeight(img.width, img.height);
-          this.list2.pictureURL.push(img.src);
-          console.log('put in list2');
-        } else {
-          this.list3.totalHeight += this.calculateImageHeight(img.width, img.height);
-          this.list3.pictureURL.push(img.src)
-          console.log('put in list3');
+      //loading picture function
+      imgload(pictureItem, callback) {
+        var img = new Image();
+        img.src = pictureItem.picture_address;
+        // if (img.complete) {
+        //   //图片是否存在缓存
+        //   callback(img);
+        //   return;
+        // }
+        img.onload = function () {
+          callback(img, pictureItem);
         }
       },
 
+      calculateImageHeight(width, height) {
+        console.log("宽度是: " + width, "高度是: " + height);
+        console.log("相对高度是: " + ((this.pictureWidth) * height / width));
+        return ((this.pictureWidth) * height / width);
+      },
 
-      //mydebug
-      mydebug() {
-        console.log("displayItem里的内容是: ");
-        console.log("list1长度为: " + this.list1.pictureURL.length+ " list1的内容是: " + this.list1.pictureURL);
-        console.log("list2长度为: " + this.list2.pictureURL.length+ " list2的内容是: " + this.list2.pictureURL);
-        console.log("list3长度为: " + this.list3.pictureURL.length+ " list3的内容是: " + this.list3.pictureURL);
-        },
+      allocPicture(img, pictureItem) {
+        let _this = this;
+        console.log('图片信息为: src——' + img.src + ", height——" + img.height + ';');
+        //输出调试信息
+        for (let i = 0; i < _this.lists.length; i++) {
+          console.log('待插入的列表的信息为,list' + i + ": " + this.list1.totalHeight);
+        }
+
+        //1.计算那一列是最短的
+        let insertColumn = 0;
+        let minHeight = _this.lists[0].totalHeight; //不能没有列
+        for (let i = 0; i < _this.lists.length; i++) {
+          if (_this.lists[i].totalHeight < minHeight) {
+            insertColumn = i;
+            minHeight = _this.lists[i].totalHeight;
+          }
+        }
+
+        //2.将图片信息插入最短的一列，并更新列总高度
+        _this.lists[insertColumn].totalHeight += _this.calculateImageHeight(img, width, img.height);
+        _this.lists[insertColumn].picturesMessage.push({//使用push使得template可以监听到最想数组的变化
+          width: img.width,//宽度高度都无用，因为有变换
+          height: img.height,
+          picture: pictureItem,
+          collection: {//添加收藏信息至list  由于未登录，所以所有都无收藏
+            user_id: -1,
+            picture_id: -1,
+            CollectTime: null,
+          }
+        })
+        console.log("put picture in list" + insertColumn);
+
+        // let isCollected = false;
+        // for(let i=0; i<_this.collectionItems; i++){
+        //   if(_this.collectionItems[i].picture_id == pictureItem.picture_id){
+        //     //
+        //   }
+        // }
+      },
+
+
+      // //mydebug
+      // mydebug() {
+      //   console.log("displayItem里的内容是: ");
+      //   console.log("list1长度为: " + this.list1.pictureURL.length + " list1的内容是: " + this.list1.pictureURL);
+      //   console.log("list2长度为: " + this.list2.pictureURL.length + " list2的内容是: " + this.list2.pictureURL);
+      //   console.log("list3长度为: " + this.list3.pictureURL.length + " list3的内容是: " + this.list3.pictureURL);
+      // },
 
       //axios function
       search() {
@@ -143,40 +220,44 @@
         if (_this.$route.params.type == 'keyword') {
           let keyword = _this.$route.params.keyword;
           _this.selectByKeyword(keyword);
-        }else {
+        } else {
 
         }
       },
 
-      selectAll() {//性能大损失
+      /***
+       * 处理全图片搜集部分
+       */
+      selectAll() {
         console.log('prepareing selectAll axios...')
+
+        let _this = this;
+        //1.axios获得所有图片
         axios.get('/pictures/selectAll').then((response) => {
-          console.log('before selectAll axios...')
-          this.displayItems = response.data;
-          console.log(this.displayItems);
-          this.initialsubList();
-          //load all pictures
-          for (let i = 0; i < this.displayItems.length; i++) {
-            this.imgload(this.displayItems[i].picture_address, this.allocPicture);
+          _this.displayItems = response.data;
+          console.log("所有的图片信息为：" + _this.displayItems);
+          // //2.axios获得收藏信息
+          // axios.get('').then(function (response) {
+          //   _this.collectionItems = response.data;
+          //   console.log("所有图片的收藏信息为：" + _this.collectionItems);
+          //   //3.列表初始化
+          //   _this.initialsubList();
+          //   //4.对图片进行分配
+          //   for (let i = 0; i < _this.displayItems.length; i++) {
+          //     _this.imgload(_this.displayItems[i], _this.allocPicture);
+          //   }
+          //   console.log('finish load list');
+          // }).catch(function (error) {
+          //   console.log(error);
+          // })
+          //2.列表初始化
+          _this.initialsubList();
+          //3.分配图片
+          for (let i = 0; i < _this.displayItems.length; i++) {
+            _this.imgload(_this.displayItems[i], _this.allocPicture);
           }
-          console.log('finish load list?');
         }).catch(function (error) {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-          }
-          console.log(error.config);
+          console.log(error);
         });
       },
 
@@ -213,10 +294,51 @@
           }
           console.log(error.config);
         });
+      },
+
+      /***
+       * 处理图片收藏部分
+       */
+      selectAllCollections() {
+        let _this = this;
+        let nowUserID = _this.$store.state.user.userID;
+        //1.axios请求获得所有当前用户收藏信息
+        axios.get('', {
+          params: {
+
+          }
+        }).then(function (response) {
+            console.log("获取收藏信息成功...");
+            _this.collectionItems = response.data;
+
+            //2.加载用户收藏信息,即对用户收藏信息进行分配
+            for(let i = 0;i<_this.collectionItems.length; i++){
+              _this.allocCollectionMessage(_this.collectionItems[i]);
+            }
+        }).catch(function (error) {
+          console.log(error);
+        })
+      },
+
+      allocCollectionMessage(collectionItem){
+        let _this = this;
+        //1.从所有的list中寻找与当前collectionItem图片id相同的一项
+        for(let i = 0;i<_this.lists.length;i++){
+          for(let j=0;j<_this.lists[i].picturesMessage.length; i++){//双循环，笨死了，但是无哈希
+            if(_this.lists[i].picturesMessage[j].picture.painter_id == collectionItem.picture_id){
+              //2.找到收藏项所在pagelist中的位置后，将collectoin信息进行替换
+              let newPictureMessage = _this.lists[i].picturesMessage[j];
+              newPictureMessage.collection = collectionItem;
+              _this.lists[i].picturesMessage.splice(j,1,newPictureMessage);
+            }
+          }
+        }
       }
     },
 
+
     created() {
+      this.startInit(3);
       this.selectAll();
       console.log('finish selectAll');
     }
@@ -224,38 +346,17 @@
 </script>
 
 <style scoped>
-  .el-row {
-    margin-bottom: 20px;
-
-  }
   :last-child {
     margin-bottom: 0;
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-
-  .bg-purple-dark {
-    background: #ffffff;
   }
 
   .bg-purple {
     background: #ffffff;
   }
 
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-
-
   .grid-content {
     border-radius: 4px;
     min-height: 24px;
-  }
-
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
   }
 
   .flex {
@@ -275,43 +376,4 @@
     overflow: hidden;
   }
 
-  .item-content {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    border-radius: 16px;
-    overflow: hidden;
-  }
-
-  img {
-    width: 100%;
-    height: 100%;
-    transition: opacity 0.3s, transform 0.3s ease;
-    object-fit: cover;
-    border-radius: 16px;
-  }
-
-  .count {
-    position: absolute;
-    display: inline-block;
-    top: 8px;
-    right: 8px;
-    color: white;
-    background-color: #0000009e;
-    padding: 2px;
-    border-radius: 4px;
-  }
-
-  img {
-    float: left;
-    fill: white;
-    height: 20px;
-    width: 20px;
-  }
-
-  span {
-    float: right;
-    padding: 0 2px;
-    line-height: 20px;
-  }
 </style>
--- src/components/picturelist/PictureItem.vue
+++ src/components/picturelist/PictureItem.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="itemContent">
-    <el-image
-      :src="itemurl"
-      class="image"></el-image>
+  <div class="item">
+    <div class="item-content">
+      <img :src="itemurl" class="item-img">
+      <el-dropdown>
+        <i class="el-icon-star-off"></i>
+        <!--      <Like/>-->
+        <!-- <el-dropdown-menu slot="dropdown"></el-dropdown-menu> -->
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
@@ -10,14 +15,29 @@
   export default {
     name: "PictureItem",
 
-    props: {
-      width: Number,
-      height: Number,
-      itemurl: String,
+    props: {//获取pagelist所传递的图片信息参数，以及是否收藏参数
+      picturesMessage:{
+        width: Number,
+        height: Number,
+        picture: {
+          picture_id: Number,
+          painter_id: Number,
+          picture_address: String,
+          uploadTime: String,
+          title: String,
+        },
+        collection: {
+          user_id: -1,
+          picture_id: -1,
+          CollectTime: String,
+        }
+      }
     },
 
     data() {
-      return {};
+      return {
+        isCollected: Boolean,
+      };
     },
 
     computed: {//计算样式
@@ -25,26 +45,44 @@
     },
 
     methods: {
+      //提交收藏需要图片id与用户id
+      submitCollect(pictureID, userID){
+        //axios提交
+      },
 
+      //取消收藏
+      cancelCollect(pictureID, userID){
+
+      },
     },
   }
 
 </script>
 
 <style scoped>
-  img {
+  .item {
+    box-sizing: border-box;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 8px;
+    padding-bottom: 16px;
+    overflow: hidden;
+  }
+  .item-content {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    border-radius: 16px;
+    overflow: hidden;
+  }
+
+  .item-img{
     width: 100%;
     height: 100%;
     transition: opacity 0.3s, transform 0.3s ease;
     object-fit: cover;
     border-radius: 16px;
-  }
-
-  .itemContent{
-    /*position: relative;*/
-    /*width: 100%;*/
-    /*height: 100%;*/
-    border-radius: 16px;
-    overflow: hidden;
   }
 </style>
